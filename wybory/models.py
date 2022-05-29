2901514fbc216747b2f4d3340019ea67ff528803
rzymski <83671265+rzymski@users.noreply.github.com>
2022-05-29 19:24:52 +0200

    Update models.py

--- wybory/models.py
+++ wybory/models.py
@@ -7,21 +7,25 @@ class Wybory(models.Model):
     poczatekWyborow = models.DateTimeField()
     koniecWyborow = models.DateTimeField()
     maxKandydatow = models.IntegerField()
+    def __str__(self):
+        return f"{self.nazwa} -max: {self.maxKandydatow} kandydatow"
 
 
 class Osoba(models.Model):
     imie = models.CharField(max_length=20)
     nazwisko = models.CharField(max_length=50)
     pesel = models.CharField(max_length=11)
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko} {self.pesel[:3]+'*****'+self.pesel[-3:]}"
 
 
 class OsobaWybory(models.Model):
     wyboryId = models.ForeignKey(Wybory, on_delete=models.CASCADE)
-    OsobaId = models.ForeignKey(Osoba, on_delete=models.CASCADE)
+    osobaId = models.ForeignKey(Osoba, on_delete=models.CASCADE)
     czyOddalGlos = models.BooleanField(default=False)
     czyKandydat = models.BooleanField(default=False)
 
 
 class Glos(models.Model):
     wyboryId = models.ForeignKey(Wybory, on_delete=models.CASCADE)
-    kandydatOsobaID = models.ForeignKey(Osoba, on_delete=models.CASCADE)
+    kandydatOsobaId = models.ForeignKey(Osoba, on_delete=models.CASCADE)
